d20be4a21820cdd2b31d1750a13e491976bc58a8
Loan Laux <contact@loanlaux.fr>
2018-01-11 16:14:10 +0100

    Add getCurrentPartOfDay method

---
+++ index.js
@@ -0,0 +1,13 @@
+exports.getCurrentPartOfDay = function() {
+  const currentHour = new Date().getHours();
+
+  let partOfDay = "MORNING";
+
+  if (currentHour > 12 && currentHour < 18) {
+    partOfDay = "AFTERNOON";
+  } else if (currentHour > 18 || currentHour < 5) {
+    partOfDay = "EVENING";
+  }
+  
+  return partOfDay;
+}
